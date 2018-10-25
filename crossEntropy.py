9d0a7c9578e82fbee3cad2486d9a0b6a57e6d18f
Atreya Shankar <35427332+AtreyaSh@users.noreply.github.com>
2018-10-25 17:44:16 +0200

    new repo

---
+++ crossEntropy.py
@@ -0,0 +1,30 @@
+from mpl_toolkits import mplot3d
+from gifMkr import gifMkr
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+def f(x, y):
+    return np.log((np.power(1-x,y-1))/np.power(x,y))
+
+x = np.linspace(0.01, 0.99, 30)
+y = np.linspace(0.01, 0.99, 30)
+
+X, Y = np.meshgrid(x, y)
+Z = f(X, Y)
+
+g = 5
+# need to define granularity and number of iterations follow from there
+
+for i in range(int(360/g)):
+	fig = plt.figure()
+	ax = plt.axes(projection='3d')
+	ax.plot_surface(X, Y, Z, rstride=5, cstride=3, cmap='viridis', edgecolor='none')
+	ax.set_xlabel('x')
+	ax.set_ylabel('y')
+	ax.set_zlabel('z')
+	ax.view_init(30, g*i)
+	fig.savefig('img/test' + str(i) + ".png")
+	plt.close(fig)
+
+gifMkr(impath = os.getcwd()+"/img", gifpath = os.getcwd(), giftitle = "/crossEntropy.gif")
